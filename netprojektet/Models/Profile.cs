574f82fe788941af9d7f4c8019f07e0015d2b551
rasoster <96240986+rasoster@users.noreply.github.com>
2022-12-19 10:53:55 +0100

    push

---
+++ netprojektet/Models/Profile.cs
@@ -0,0 +1,20 @@
+﻿namespace netprojektet.Models
+{
+    public class Profile
+    {
+        public int ProfileID { get; set; }
+
+        public string PicUrl { get; set; }
+
+        public string FirstName { get; set;}
+
+        public string LastName { get; set;}
+        public int Visitors { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public bool PrivateProfile { get; set; }
+
+        public User User { get; set; }
+
+    }
+}
---
+++ netprojektet/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace netprojektet.Models
+{
+    public class User
+    {
+        public string UserID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public int ProfileID { get; set; }
+
+        [ForeignKey (nameof(ProfileID))]
+        public Profile profile { get; set; }
+    }
+}
