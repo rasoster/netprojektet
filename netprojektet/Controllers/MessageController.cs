3d136a6b73f6a2c93f1b2d18665f83dad00e9dbc
Rebecca <mmmolnstrand@gmail.com>
2022-12-28 13:50:25 +0100

    La till message view, och message controller utanför API

---
+++ netprojektet/Controllers/MessageController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+
+using DataAccessLayer;
+using Models;
+
+namespace netprojektet.Controllers
+{
+    public class MessageController : Controller
+    {
+        private LinkedoutDbContext _DbContext;
+
+        public MessageController(LinkedoutDbContext linkedoutDbContext)
+        {
+            _DbContext = linkedoutDbContext;
+        }
+        public IActionResult Message()
+        {
+
+
+            return View("Message");
+
+        }
+    }
+}
