7111a8b3f0bc3e782e44f924ce4bcd6513411eb3
Rebecca <mmmolnstrand@gmail.com>
2023-01-03 12:53:41 +0100

    Ta bort experience

---
+++ DataLayer/ExperienceViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class ExperienceViewModel
+    {
+        public Experience experience { get; set; }
+
+        public ProfileHasExperience profileHasExperience { get; set; }
+    }
+}
