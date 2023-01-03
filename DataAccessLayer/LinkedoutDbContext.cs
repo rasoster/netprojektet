07644df7996d068b5916288889ecb1203caeb3e0
rasoster <96240986+rasoster@users.noreply.github.com>
2023-01-03 15:02:57 +0100

    uppdatering

--- DataAccessLayer/LinkedoutDbContext.cs
+++ DataAccessLayer/LinkedoutDbContext.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Models;
 using Microsoft.EntityFrameworkCore.Internal;
